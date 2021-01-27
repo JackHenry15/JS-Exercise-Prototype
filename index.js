cfb7e8bbe88ccd8468bafb38183c0507ebc2aac5
Jack Henry <jackson.lewis.henry@gmail.com>
2021-01-27 14:33:02 -0500

    working on car

--- index.js
+++ index.js
@@ -39,10 +39,29 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
   }
  
+  Person.prototype.eat = function(edible){
+    if(this.stomach.length < 10){
+      this.stomach.push(edible);
+    }
+  }
+
+  Person.prototype.poop = function(){
+    this.stomach = [];
+  }
+
+  Person.prototype.toString = function(){
+    return `${this.name}, ${this.age}`;
+  }
+
+  const myNameJeff = new Person('wahoo', 24);
+
+  console.log(myNameJeff.toString());
  
 
   
@@ -63,8 +82,17 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
+ function Car(model, milesPerGallon) {
+   this.model = model;
+   this.milesPerGallon = milesPerGallon;
+   this.tank = 0;
+   this.odometer = 0;
+
     
+  }
+
+  Car.prototype.fill = function(gallons){
+    this.tank = (tank + gallons);
   }
   
   
